d77cb3d5311d9cd86c4afddc535ca7cba33ea04a
Aefer <Aefer@localdomain>
2020-03-24 09:57:31 +0800

    update

--- common/rules_update.sh
+++ common/rules_update.sh
@@ -4,7 +4,7 @@ rm -rf $TMPDIR/rules $TMPDIR/rules.tar.xz
 mkdir $TMPDIR/rules
 cd $TMPDIR/rules
 
-#Update configuration file
+#Update config file
 wget --retry-connrefused --waitretry=2 -qO- https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf | sed 's/=\/./ \//' - | sed 's/0.0.0.0/#/' - > yhosts_union.ad.conf.tmp &
 pid1=$!
 wget --retry-connrefused --waitretry=2 -O yhosts_ip.conf.tmp https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/ip.conf &
@@ -29,6 +29,7 @@ wait $pid7
 sed -i '/^#/d' *.conf.tmp
 sed -i 's/=/ /' *.conf.tmp
 
+#Update hosts
 wget --retry-connrefused --waitretry=2 -O adguard.hosts.tmp https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardDNS.txt &
 pid1=$!
 wget --retry-connrefused --waitretry=2 -O Peter_Lowe.hosts.tmp 'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext' &
@@ -41,10 +42,8 @@ wget --retry-connrefused --waitretry=2 -O adwars.hosts.tmp https://raw.githubuse
 pid5=$!
 wget --retry-connrefused --waitretry=2 -O neohosts.hosts.tmp 'https://cdn.jsdelivr.net/gh/neoFelhz/neohosts@gh-pages/full/hosts' &
 pid6=$!
-wget --retry-connrefused --waitretry=2 -O hphosts.hosts.tmp https://hosts-file.net/ad_servers.txt &
-pid7=$!
 wget --retry-connrefused --waitretry=2 -O adaway.hosts.tmp https://adaway.org/hosts.txt &
-pid8=$!
+pid7=$!
 wait $pid1
 wait $pid2
 wait $pid3
@@ -52,15 +51,29 @@ wait $pid4
 wait $pid5
 wait $pid6
 wait $pid7
-wait $pid8
 cat *.hosts.tmp > all.hosts.tmp
 dos2unix all.hosts.tmp
 sed -i '/^#/d' all.hosts.tmp
 sed -i 's/127.0.0.1/#/' all.hosts.tmp
 sed -i 's/0.0.0.0/#/' all.hosts.tmp
 sed -i 's/::/#/' all.hosts.tmp
-sort -u all.hosts.tmp *.ad.conf.tmp  | awk '/^#/ {printf"address /%s/%s\n",$2,$1}' > all_block.conf
-rm -f *.ad.conf.tmp
+sed -i 's/\t/ /g' all.hosts.tmp
+sort -u all.hosts.tmp *.ad.conf.tmp  | awk '/^#/ {printf"address /%s/%s\n",$2,$1}' > all_block.conf.tmp
+
+#whitelist
+wget --retry-connrefused --waitretry=2 -qO- https://github.com/vokins/yhosts/raw/master/data/moot/cps.txt | sed '/^@/d' - > cps.txt
+cat *.txt > whitelist.txt
+sed -i '/^#/d' whitelist.txt
+sed -i 's/\t/ /g' whitelist.txt
+sed -i 's/127.0.0.1 //' whitelist.txt
+sed -i 's/0.0.0.0 //' whitelist.txt
+sed -i 's/:: //' whitelist.txt
+for white in \$(cat whitelist.txt)
+do
+  sed -i '/white/d' all_block.conf.tmp
+done
+
+rm -f *.ad.conf.tmp *.txt
 rename 's/\.tmp$//' *.conf.tmp
 
 rm -f *.tmp wget-log*
